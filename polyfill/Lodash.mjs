3185354900bfd5bf7f9feb8b04ece2e5f755e418
脆薯饼 <Virgil@nanocat.me>
2024-12-12 17:14:06 +0800

    feat: Lodash
    Update Lodash.mjs

--- polyfill/Lodash.mjs
+++ polyfill/Lodash.mjs
@@ -1,5 +1,16 @@
 /* https://www.lodashjs.com */
 export class Lodash {
+	static escape(string) {
+		const map = {
+			"&": "&amp;",
+			"<": "&lt;",
+			">": "&gt;",
+			'"': "&quot;",
+			"'": "&#39;",
+		};
+		return string.replace(/[&<>"']/g, m => map[m]);
+	}
+
 	static get(object = {}, path = "", defaultValue = undefined) {
 		// translate array case to dot case, then split with .
 		// a[0].b -> a.0.b -> ['a', '0', 'b']
@@ -11,10 +22,40 @@ export class Lodash {
 		return result === undefined ? defaultValue : result;
 	}
 
+	static omit(object = {}, paths = []) {
+		if (!Array.isArray(paths)) paths = [paths.toString()];
+		paths.forEach(path => Lodash.unset(object, path));
+		return object;
+	}
+
+	static pick(object = {}, paths = []) {
+		if (!Array.isArray(paths)) paths = [paths.toString()];
+		const filteredEntries = Object.entries(object).filter(([key, value]) => paths.includes(key));
+		return Object.fromEntries(filteredEntries);
+	}
+
 	static set(object, path, value) {
 		if (!Array.isArray(path)) path = Lodash.toPath(path);
 		path.slice(0, -1).reduce((previousValue, currentValue, currentIndex) => (Object(previousValue[currentValue]) === previousValue[currentValue] ? previousValue[currentValue] : (previousValue[currentValue] = /^\d+$/.test(path[currentIndex + 1]) ? [] : {})), object)[path[path.length - 1]] = value;
 		return object;
+	}
+
+	static toPath(value) {
+		return value
+			.replace(/\[(\d+)\]/g, ".$1")
+			.split(".")
+			.filter(Boolean);
+	}
+
+	static unescape(string) {
+		const map = {
+			"&amp;": "&",
+			"&lt;": "<",
+			"&gt;": ">",
+			"&quot;": '"',
+			"&#39;": "'",
+		};
+		return string.replace(/&amp;|&lt;|&gt;|&quot;|&#39;/g, m => map[m]);
 	}
 
 	static unset(object = {}, path = "") {
@@ -27,34 +68,5 @@ export class Lodash {
 			return Object(previousValue)[currentValue];
 		}, object);
 		return result;
-	}
-
-	static toPath(value) {
-		return value
-			.replace(/\[(\d+)\]/g, ".$1")
-			.split(".")
-			.filter(Boolean);
-	}
-
-	static escape(string) {
-		const map = {
-			"&": "&amp;",
-			"<": "&lt;",
-			">": "&gt;",
-			'"': "&quot;",
-			"'": "&#39;",
-		};
-		return string.replace(/[&<>"']/g, m => map[m]);
-	}
-
-	static unescape(string) {
-		const map = {
-			"&amp;": "&",
-			"&lt;": "<",
-			"&gt;": ">",
-			"&quot;": '"',
-			"&#39;": "'",
-		};
-		return string.replace(/&amp;|&lt;|&gt;|&quot;|&#39;/g, m => map[m]);
 	}
 }
