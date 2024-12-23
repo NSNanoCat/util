edc5a3e58c214c4207f638b3ce747b35b49b3779
脆薯饼 <Virgil@nanocat.me>
2024-12-23 14:40:44 +0800

    fix: index

--- polyfill/index.js
+++ polyfill/index.js
@@ -1,5 +1,5 @@
 export * from "./Console.mjs";
 export * from "./fetch.mjs";
 export * from "./Lodash.mjs";
-export * from "./StatusCodes.mjs";
+export * from "./StatusTexts.mjs";
 export * from "./Storage.mjs";
